5d917518f5947a10dddede5e6327bc7ac48443d1
marcelocaser <marcelocaser@gmail.com>
2020-10-06 17:06:28 -0300

    projeto inicial trabalho pratico galeria-fotos

---
+++ src/app/galeria-component/galeria-component.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria-component',
+  templateUrl: './galeria-component.component.html',
+  styleUrls: ['./galeria-component.component.css'],
+})
+export class GaleriaComponentComponent {}
