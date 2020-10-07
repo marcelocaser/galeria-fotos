9a3c3f14574da3383865070ccce47aa91342bcff
marcelocaser <marcelocaser@gmail.com>
2020-10-06 23:06:19 -0300

    implementacao concluida, sem estilização;

--- src/app/galeria-component/galeria-component.component.ts
+++ src/app/galeria-component/galeria-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-galeria-component',
@@ -6,6 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./galeria-component.component.css'],
 })
 export class GaleriaComponentComponent {
-  titulo: string = 'Teste';
-  pictures: string[] = ['']; 
+  currentPicture: number = 0;
+  @Input() title: string;
+  @Input() pictures: string[] = [''];
+
+  nextPicture() {
+    this.currentPicture++;
+  }
+
+  previousPicture() {
+    this.currentPicture--;
+  }
+
+  firstPicture() {
+    this.currentPicture = 0;
+  }
+
+  lastPicture() {
+    this.currentPicture = this.pictures.length - 1;
+  }
+
+  validateNextAndLastPage(): boolean {
+    return this.currentPicture === this.pictures.length - 1 || this.pictures.length === 0;
+  }
 }
